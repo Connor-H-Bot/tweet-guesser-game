a8b4b6d62daafa346dc72b0e453501211783380c
Connor_Bot <60994277+Connor-H-Bot@users.noreply.github.com>
2024-10-12 17:23:13 +0200

    Updated App Logic and Displays
    Changed the CSS so its more obvious when a bad answer was selected, and now it looks better on mobile devices.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive"; // Import react-responsive
 import "./App.css"; // Make sure to add styles here or edit existing CSS file
 
 const TweetsGame = () => {
@@ -6,6 +7,12 @@ const TweetsGame = () => {
   const [tweet2, setTweet2] = useState(null);
   const [loading, setLoading] = useState(true);
   const [counter, setCounter] = useState(0);
+  const [borderColor, setBorderColor] = useState("blue");
+  const [isShaking, setIsShaking] = useState(false);
+  const [highlightedTweet, setHighlightedTweet] = useState(null);
+
+  // Check if the screen width is mobile size
+  const isMobile = useMediaQuery({ query: "(max-width: 600px)" });
 
   // Function to fetch two tweets from the API
   const fetchTweets = async () => {
@@ -42,16 +49,36 @@ const TweetsGame = () => {
     }
   };
 
-  // Function to handle tweet selection (sends API call)
   const handleTweetClick = async (tweet) => {
-    if (tweet.type == "fake") {
+    if (tweet.type === "fake") {
       setCounter(0);
+      setBorderColor("red"); // Set border to red on wrong guess
+      setIsShaking(true); // Enable shaking animation
+      setHighlightedTweet(tweet.id); // Set highlighted tweet to red
+  
+      // Reset shaking and highlighted tweet after a short duration
+      setTimeout(() => {
+        setIsShaking(false);
+        setHighlightedTweet(null); // Reset highlight after showing for a bit
+      }, 1000); // Keep tweet highlighted for 1 second
     } else {
-      setCounter(counter + 1);
+      const newCounter = counter + 1;
+      setCounter(newCounter);
+      setBorderColor("green"); // Change border to green on correct guess
+      setHighlightedTweet(tweet.id); // Set highlighted tweet to green
+  
+      // Reset highlighted tweet after a short duration
+      setTimeout(() => {
+        setHighlightedTweet(null); // Reset highlight after showing for a bit
+      }, 1000); // Keep tweet highlighted for 1 second
     }
-
-    await fetchTweets();
+  
+    // Introduce a delay before fetching new tweets
+    setTimeout(async () => {
+      await fetchTweets(); // Fetch new tweets
+    }, 300); // Delay of 0.3 seconds
   };
+  
 
   // Fetch tweets on component mount
   useEffect(() => {
@@ -63,8 +90,19 @@ const TweetsGame = () => {
   }
 
   const Tweet = ({ tweetData, handleClick }) => {
+    const isHighlighted = highlightedTweet === tweetData.id; // Check if this tweet is highlighted
+
     return (
-      <div onClick={handleClick} className="button unselectable flip-card">
+      <div
+        onClick={handleClick}
+        className={`button unselectable flip-card ${
+          isHighlighted
+            ? tweetData.type === "fake"
+              ? "highlight-red"
+              : "highlight-green"
+            : ""
+        }`}
+      >
         <div className="tweet-wrap flip-card-inner hover-outline">
           <div className="flip-card-front">
             <div className="tweet-header">
@@ -82,82 +120,7 @@ const TweetsGame = () => {
                   style={{ verticalAlign: "middle", paddingLeft: "5px" }}
                 ></object>
                 <span>@{tweetData.handle}</span>
-                <span>{tweetData.date}</span>
-                <p>{tweetData.body}</p>
-              </div>
-            </div>
-            <div className="tweet-info-counts">
-              <div className="comments">
-                <svg
-                  class="feather feather-message-circle sc-dnqmqq jxshSx"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  aria-hidden="true"
-                >
-                  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
-                </svg>
-              </div>
-              <div className="retweets">
-                <svg
-                  class="feather feather-repeat sc-dnqmqq jxshSx"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  aria-hidden="true"
-                >
-                  <polyline points="17 1 21 5 17 9"></polyline>
-                  <path d="M3 11V9a4 4 0 0 1 4-4h14"></path>
-                  <polyline points="7 23 3 19 7 15"></polyline>
-                  <path d="M21 13v2a4 4 0 0 1-4 4H3"></path>
-                </svg>
-              </div>
-              <div className="likes">
-                <svg
-                  class="feather feather-heart sc-dnqmqq jxshSx"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  aria-hidden="true"
-                >
-                  <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-                </svg>
-              </div>
-              <div className="message">
-                <svg
-                  class="feather feather-send sc-dnqmqq jxshSx"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  aria-hidden="true"
-                >
-                  <line x1="22" y1="2" x2="11" y2="13"></line>
-                  <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
-                </svg>
+                <p>{formatTweetText(tweetData.body)}</p>
               </div>
             </div>
           </div>
@@ -184,24 +147,78 @@ const TweetsGame = () => {
     body: tweet2.text,
   };
 
+  const formatTweetText = (text) => {
+    // Step 1: Replace | with ,
+    let formattedText = text.replace(/\|/g, ",");
+
+    // Step 2: Replace &amp with &
+    formattedText = formattedText.replace(/&amp/g, "&");
+
+    // Step 3: Format Twitter handles (underline and make blue)
+    const handleRegex = /(^|[\s\W])@(\w+)(:)?/g; // Make the colon part of the captured groups
+    formattedText = formattedText.replace(
+      handleRegex,
+      (match, before, handle, colon) =>
+        `${before}<span style="color: #1DA1F2; margin-left: 0px; margin-right: 0px; font-style: normal;">@${handle}</span>${
+          colon || ""
+        }`
+    );
+
+    // Step 4: Replace \" with " and italicize text inside quotes
+    const quoteRegex = /\\"(.*?)\\"/g;
+    formattedText = formattedText.replace(
+      quoteRegex,
+      (match, p1) => `<i>"${p1}"</i>`
+    );
+
+    // Return as JSX with 'dangerouslySetInnerHTML'
+    return <p dangerouslySetInnerHTML={{ __html: formattedText }} />;
+  };
+
+  // Use different layouts based on screen size
   return (
     <div className="Game" id="mainScreen">
       <div className="main_div center">
-        <div className="counter-container">
-          <span className="counter-value">{counter}</span>
+        <div
+          className="counter-container"
+          style={{
+            border: `4px solid ${borderColor}`, // Adjust thickness here
+            transition: "border-color 0.3s ease", // Smooth transition
+          }}
+        >
+          <span className={`counter-value ${isShaking ? "shake" : ""}`}>
+            {counter}
+          </span>
         </div>
-        <div className="left">
-          <Tweet
-            tweetData={tweetDataLeft}
-            handleClick={() => handleTweetClick(tweet1)}
-          />
-        </div>
-        <div className="right">
-          <Tweet
-            tweetData={tweetDataRight}
-            handleClick={() => handleTweetClick(tweet2)}
-          />
-        </div>
+
+        {/* Conditional rendering based on screen size */}
+        {isMobile ? (
+          <div className="tweets-container-mobile">
+            <Tweet
+              tweetData={tweetDataLeft}
+              handleClick={() => handleTweetClick(tweet1)}
+            />
+            <Tweet
+              tweetData={tweetDataRight}
+              handleClick={() => handleTweetClick(tweet2)}
+            />
+          </div>
+        ) : (
+          <div className="tweets-container-desktop">
+            <div className="left">
+              <Tweet
+                tweetData={tweetDataLeft}
+                handleClick={() => handleTweetClick(tweet1)}
+              />
+            </div>
+            <div className="right">
+              <Tweet
+                tweetData={tweetDataRight}
+                handleClick={() => handleTweetClick(tweet2)}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
