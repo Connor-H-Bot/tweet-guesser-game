da276076e14e2991303ffca45bf790af814cb3c2
Connor <60994277+Connor-H-Bot@users.noreply.github.com>
2024-11-06 07:17:04 +0100

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -8,9 +8,3 @@ source venv/bin/activate
 
 # Start Django server in the background with nohup
 nohup python3 manage.py runserver 0.0.0.0:8000 &
-
-# Navigate to frontend directory
-cd /home/trumpter_admin/tweet_guesser_v2/tweet-guesser-game/frontend
-
-# Start the frontend server in the background with nohup
-nohup serve -s build &
